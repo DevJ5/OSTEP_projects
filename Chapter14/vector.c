e8966fb71b6318975d0d574ea438f4ffac9ef88e
DevJ5 <idevelopjs@gmail.com>
2020-06-04 09:13:04 +0200

    dynamic vectors

--- Chapter14/vector.c
+++ Chapter14/vector.c
@@ -1,10 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct {
+    int* array;
+    size_t used;
+    size_t size;
+} Array;
+
 int main (void)
 {
-    int* vector = malloc(2 * sizeof(int));
-    int n;
-    scanf("%d", &n);
-    printf("%d\n", n);
+    Array arr;
+    arr.array = malloc(8 *  sizeof(int));
+    arr.array[0] = 100;
+    arr.used = 0;
+    arr.size = 8;
+    for (int i = 0; i < 100; i++)
+    {
+        if (arr.used == arr.size)
+        {
+            arr.size *= 2;
+            arr.array = (int *) realloc(arr.array, arr.size * sizeof(int));
+        }
+        arr.array[arr.used] = i;
+        arr.used++;
+    }
+    printf("%d\n", arr.array[0]);
+    printf("%ld\n", arr.size);
+    free(arr.array);
+
 }
